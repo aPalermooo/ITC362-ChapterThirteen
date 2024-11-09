ad99a29597513376f29dbfde75b044b27b490312
aPalermooo <ajp2s@login.missouristate.edu>
2024-11-09 17:30:29 -0600

    Exercise 3b Complete

--- app/src/main/java/com/example/chapter_ten/CrimeListAdapter.kt
+++ app/src/main/java/com/example/chapter_ten/CrimeListAdapter.kt
@@ -1,17 +1,24 @@
 package com.example.chapter_ten
 
+import android.icu.text.DateFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.chapter_ten.databinding.ListItemCrimeBinding
+import java.util.Locale
 
-class CrimeHolder( private val binding : ListItemCrimeBinding )
-    : RecyclerView.ViewHolder(binding.root) {
+
+class CrimeHolder( private val binding : ListItemCrimeBinding ) : RecyclerView.ViewHolder(binding.root) {
         fun bind (crime: Crime) {
             binding.crimeTitle.text = crime.title
-            binding.crimeDate.text = crime.date.toString()
+
+            // Exercise 3b
+            val locale = Locale.getDefault()
+            val format = DateFormat.getDateInstance(DateFormat.MEDIUM, locale)
+            val crimeDate = format.format(crime.date)
+            binding.crimeDate.text = crimeDate
 
             binding.root.setOnClickListener {
                 Toast.makeText( binding.root.context, "${crime.title} click", Toast.LENGTH_SHORT).show()
@@ -24,8 +31,7 @@ class CrimeHolder( private val binding : ListItemCrimeBinding )
         }
     }
 
-class CrimeListAdapter ( private  val crimes : List<Crime> )
-    : RecyclerView.Adapter<CrimeHolder>() {
+class CrimeListAdapter ( private  val crimes : List<Crime> ) : RecyclerView.Adapter<CrimeHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = ListItemCrimeBinding.inflate(inflater, parent, false)
