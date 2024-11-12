80d2bf48021a7114245972e44e5250567e65356c
aPalermooo <ajp2s@login.missouristate.edu>
2024-11-11 20:54:11 -0600

    Revision of Dependencies

--- app/src/main/java/com/example/chapter_ten/CrimeListViewModel.kt
+++ app/src/main/java/com/example/chapter_ten/CrimeListViewModel.kt
@@ -1,9 +1,13 @@
 package com.example.chapter_ten
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import java.util.*
+
+private const val TAG = "CrimeListViewModel"
 
 class CrimeListViewModel : ViewModel() {
 
@@ -11,7 +15,13 @@ class CrimeListViewModel : ViewModel() {
 
     init {
 
+        Log.d(TAG, "init starting")
+
         viewModelScope.launch {
+
+            Log.d(TAG, "coroutine started")
+            delay(5000)
+
             for (i in 0 until 100) {
                 val crime = Crime(
                     id = UUID.randomUUID(),
@@ -22,6 +32,8 @@ class CrimeListViewModel : ViewModel() {
 
                 crimes += crime
             }
+
+            Log.d(TAG, "Loading crimes finished")
         }
     }
 }
